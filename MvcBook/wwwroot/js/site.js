108a93881ed34ad69abe36c9b285d9b9ad03f808
Азат <freedoom9654@gmail.com>
2020-10-09 17:41:48 +0300

    Добавлен модуль  для ссылки "Create". Добавлен модуль для ссылки имени автора. Исправлен случай вызова Details когда поле названия книги пуст. Изменены цвета иконок при разных состояниях активности.

--- MvcBook/wwwroot/js/site.js
+++ MvcBook/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).on('click', '.sub', function (e) {
+    e.preventDefault();
+
+    var $this = $(this);
+    var form = $this.parents('.modal').find('form');
+    var dataToSend = form.serialize();
+    var url = form.attr('action');
+
+    $.post(url, dataToSend, function (data) {
+        if (!data.includes('modal-content')) {
+            $('#modDialog').modal('hide');
+            location.reload(true);
+            return;
+        }
+
+
+        $('#dialogContent').html(data);
+        $('#modDialog').modal('show');
+    });
+});
+
+$(function () {
+    $.ajaxSetup({ cache: false });
+    $(".openmodal").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modDialog').modal('show');
+        });
+    });
+})
